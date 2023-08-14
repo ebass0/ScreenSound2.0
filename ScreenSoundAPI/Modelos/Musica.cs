71a54a5a845240deaa26a628d19ffdd86d07704b
ebasso <ebch.soares@outlook.com>
2023-08-14 12:56:07 -0300

    Finalização do projeto em conjunto com a Alura.

--- ScreenSoundAPI/Modelos/Musica.cs
+++ ScreenSoundAPI/Modelos/Musica.cs
@@ -4,6 +4,7 @@ namespace ScreenSoundAPI.Modelos;
 
 internal class Musica
 {
+    private string[] tonalidades = { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" };
     [JsonPropertyName("song")]      //Atributo que especifica um metadado da API
     public string? Nome { get; set; }
 
@@ -16,11 +17,21 @@ internal class Musica
     [JsonPropertyName("genre")]
     public string? Genero { get; set; }
 
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+    public string Tonalidade {
+        get 
+        {
+            return this.tonalidades[Key];
+        } 
+    }
+
     public void ExibirDetalhesDaMusica()
     {
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Música: {Nome}");
         Console.WriteLine($"Duração: {Duracao / 100} segundos");
         Console.WriteLine($"Gênero musical: {Genero}");
+        Console.WriteLine($"Tonalidade: {Tonalidade}");
     }
 }
--- ScreenSoundAPI/Program.cs
+++ ScreenSoundAPI/Program.cs
@@ -8,10 +8,39 @@ using (HttpClient client = new HttpClient())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!; //Deserializa o Json para ser entendido pela aplicação em C#
-        //LinqFilter.FitrarTodosOsGenerosMusicais(musicas);
-        //LinqOrder.ExibirListaDeArtistasOrdernados(musicas);
-        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+
+        LinqFilter.FiltrarMusicasEmCSharp(musicas);
+
+        /*
+        --- TESTES --- 
+
+        musicas[0].ExibirDetalhesDaMusica();
+
+        LinqFilter.FitrarTodosOsGenerosMusicais(musicas);
+        LinqOrder.ExibirListaDeArtistasOrdernados(musicas);
+        LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
         LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Justin Bieber");
+
+        var musicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+        musicasPreferidasDoDaniel.AdcionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasDoDaniel.AdcionarMusicasFavoritas(musicas[377]);
+        musicasPreferidasDoDaniel.AdcionarMusicasFavoritas(musicas[4]);
+        musicasPreferidasDoDaniel.AdcionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasDoDaniel.AdcionarMusicasFavoritas(musicas[1467]);
+
+        musicasPreferidasDoDaniel.ExibirMusicasFavoritas();
+
+        var musicasPreferidasDaEmilly = new MusicasPreferidas("Emilly");
+        musicasPreferidasDaEmilly.AdcionarMusicasFavoritas(musicas[500]);
+        musicasPreferidasDaEmilly.AdcionarMusicasFavoritas(musicas[637]);
+        musicasPreferidasDaEmilly.AdcionarMusicasFavoritas(musicas[428]);
+        musicasPreferidasDaEmilly.AdcionarMusicasFavoritas(musicas[13]);
+        musicasPreferidasDaEmilly.AdcionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasDaEmilly.ExibirMusicasFavoritas();
+
+        musicasPreferidasDaEmilly.GerarArquivoJson();*/
+
     }
     catch (Exception ex)
     {
